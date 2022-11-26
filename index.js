fe7033d8356e443db2e92c689c003292cf692317
Marjan-001 <marjan199721@gmail.com>
2022-11-26 17:33:50 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hmvy7hf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+console.log(uri)
+
+async function run() {
+    try {
+        const categoriesCollection = client.db('laptopSwappers').collection('categories');
+        app.get('/categories', async (req, res) => {
+
+            const query = {};
+            const result = await categoriesCollection.find(query).toArray();
+
+            res.send(result);
+        })
+
+
+    }
+    finally {
+
+
+
+    }
+}
+run().catch(console.log)
+
+
+app.get('/', async (req, res) => {
+    res.send('laptop swappers server is running');
+})
+
+app.listen(port, () => console.log(`laptop swappers running on ${port}`))
